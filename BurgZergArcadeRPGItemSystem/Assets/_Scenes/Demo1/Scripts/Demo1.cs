58195c49da645ee8f0cd6c2d5b1ed40d79fd3a49
Steven B <Stevjlk@hotmail.com>
2015-07-15 01:45:56 -0400

    Part 23 Completed.

---
+++ BurgZergArcadeRPGItemSystem/Assets/_Scenes/Demo1/Scripts/Demo1.cs
@@ -0,0 +1,31 @@
+﻿//connect to database
+//spawn item from database
+
+using UnityEngine;
+using System.Collections;
+using BurgZergArcade.ItemSystem;
+
+[DisallowMultipleComponent]
+public class Demo1 : MonoBehaviour {
+
+	public ItemSystemWeaponDatabase database;
+	
+	private void OnGUI ()
+	{
+		for(int cnt = 0; cnt < database.Count; cnt++)
+		{
+			if(GUILayout.Button("Spawn: " + database.Get(cnt).Name))
+			{
+				Spawn(cnt);
+			}
+		}
+	}
+	
+	private void Spawn (int index)
+	{
+		ItemSystemWeapon isw = database.Get(index);
+		
+		GameObject weapon = Instantiate(isw.Prefab);
+		weapon.name = isw.Name;
+	}
+}
